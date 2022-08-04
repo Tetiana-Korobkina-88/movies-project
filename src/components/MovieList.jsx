907976ad253d8221833e72066916c481db4e8e44
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-04 14:18:27 +0200

    working with the API and created a list of movies

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,13 @@
+import { Movie } from './Movie';
+
+function MovieList (props) {
+  const { movies } = props;
+
+  return <div className="movies">
+    {movies.map(movie => (
+      <Movie key={movie.id} {...movie} />
+    ))}
+  </div>
+ }
+
+export { MovieList };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,24 @@
-function Main() {
-  return <main className="container  content">
-    Hello from Movies App
-  </main>
+import React from "react";
+import { MovieList } from '../components/MovieList'
 
+class Main extends React.Component {
+  state = {
+    movies: [], 
+  }
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=2f9277d6&s=matrix')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
+ 
+  render() {
+    const { movies } = this.state;
+    
+    return <main className="container  content">
+      { movies.length ? (<MovieList movies={this.state.movies} />) : <h3>Loading...</h3> }
+      </main>
+    }
 }
 
 export { Main };
