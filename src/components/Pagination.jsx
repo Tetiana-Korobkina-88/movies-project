cd1de3da97c35748b1d4fff68c84a0d238c141a5
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-10 10:42:31 +0200

    changed the class component (main) to a functional

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,22 @@
+function Pagination() {
+  return (
+    <ul class="pagination">
+      <li class="active teal lighten-1">
+        <a href="#!">1</a>
+      </li>
+      <li class="waves-effect">
+        <a href="#!">2</a>
+      </li>
+      <li class="waves-effect">
+        <a href="#!">3</a>
+      </li>
+      <li class="waves-effect">
+        <a href="#!">4</a>
+      </li>
+      <li class="waves-effect">
+        <a href="#!">5</a>
+      </li>
+    </ul>
+  );
+}
+export { Pagination };
